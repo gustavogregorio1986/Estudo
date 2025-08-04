641c01e487f3bb61c34faa056ff6c9896b7eeb30
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-08-04 09:56:28 -0300

    Estudando a logica de programação em C#, paar estar cada vez, melhor no conhecimento

---
+++ Estudo16/Program.cs
@@ -0,0 +1,6 @@
+﻿int counter = 0;
+while (counter < 5)
+{
+    Console.WriteLine($"Hello World! Counter is: {counter}");
+    counter++;
+}
---
+++ Estudo17/Program.cs
@@ -0,0 +1,7 @@
+﻿int counter = 0;
+
+do
+{
+    Console.WriteLine($"Hello World! Counter is: {counter}");
+    counter++;
+} while (counter < 5);
